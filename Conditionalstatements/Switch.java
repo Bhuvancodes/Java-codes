1283e05c25c75715fb3416a8364709e68f35978f
Bhuvancodes <bhuvan8934@gmail.com>
2024-01-20 12:36:50 -0600

    modified

--- Conditionalstatements/Switch.java
+++ Conditionalstatements/Switch.java
@@ -1,29 +1,40 @@
-import java.util.*;
 package Conditionalstatements;
-public class Switch 
-{
-  public static void main (String args[])
-  { 
-  Scanner uservalue = new Scanner (System.in);
-  int a = uservalue.nextInt();
-  switch(a)
-  {
-    case 1:
-         System.out.println("Monday");
-    case 2:
-         System.out.println("Tuesday");
-    case 3:
-         System.out.println("Wednessday");
-    case 4:
-         System.out.println("Thursday");
-    case 5:
-         System.out.println("Friday");
-    case 6:
-         System.out.println("Saturday");
-    case 7:
-         System.out.println("Sunday");
-  }
-  System.out.println("Enter Value between 1 to 7");
-  }
 
+import java.util.*
+;
+
+public class Switch
+ {
+    public static void main(String args[])
+     {
+        Scanner uservalue = new Scanner(System.in);
+        int a = uservalue.nextInt();
+        switch (a) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Enter a value between 1 to 7");
+        }
+        
+        uservalue.close(); // Close the Scanner
+    }
 }
